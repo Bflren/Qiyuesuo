b07434f11725f05262a2a887f8efb7b68e1c7c6a
Bflren <72384753+Bflren@users.noreply.github.com>
2021-10-22 13:49:45 +0800

    Add files via upload

---
+++ metadata.sql
@@ -0,0 +1,14 @@
+create table metadata.meta
+(
+    id            int auto_increment
+        primary key,
+    size          double                             null comment '文件大小',
+    type          varchar(50)                        null comment '文件类型',
+    file_name     varchar(50)                        null comment '文件名',
+    original_name varchar(50)                        null comment '原始文件名',
+    create_time   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    address       varchar(50)                        null comment '文件保存地址'
+)
+    comment '上传文件元数据';
+
+
