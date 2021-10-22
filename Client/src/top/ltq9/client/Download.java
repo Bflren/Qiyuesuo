03642c96bd8565c7bc717e877d41c0a535409ca8
Bflren <72384753+Bflren@users.noreply.github.com>
2021-10-22 13:51:23 +0800

    Update Download.java

--- Client/src/top/ltq9/client/Download.java
+++ Client/src/top/ltq9/client/Download.java
@@ -11,10 +11,12 @@ public class Download {
 
     public static void main(String[] args) throws Exception {
 
+        //下载地址 get请求 参数是生成的文件名
         URL url = new URL("http://127.0.0.1:8080/download?fileName=e83f2f36-0240-4096-8366-97f59eb085c2.pdf");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setDoInput(true);
 
+        //获得输入流，下载到本地
         InputStream inputStream = connection.getInputStream();
         File file = new File("d://upload/download/download.pdf");
         FileOutputStream outputStream = new FileOutputStream(file);
