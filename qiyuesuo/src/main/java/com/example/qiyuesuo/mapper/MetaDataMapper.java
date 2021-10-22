6e7f2d001768e83baf30f5f45ca6b01c0e704a44
Bflren <72384753+Bflren@users.noreply.github.com>
2021-10-22 13:55:13 +0800

    Update MetaDataMapper.java

--- qiyuesuo/src/main/java/com/example/qiyuesuo/mapper/MetaDataMapper.java
+++ qiyuesuo/src/main/java/com/example/qiyuesuo/mapper/MetaDataMapper.java
@@ -6,8 +6,11 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MetaDataMapper {
 
+    //上传文件，数据库新增记录
     int addRecord(MetaData metaData);
+    //下载文件，根据文件名获得本地路径
     String findPathByFileName(String fileName);
+    //根据文件名获得所有数据
     MetaData findMetaByFileName(String fileName);
 
 }
